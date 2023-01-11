584368b43b3186cbe829f9b7a0cfb08633350663
sudoku <gzcsudo@gmail.com>
2023-01-11 18:49:15 +0800

    Update frameqa.sh

--- pre_sh/frameqa.sh
+++ pre_sh/frameqa.sh
@@ -1,26 +1,26 @@
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type frameqa \
         --mode total
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type frameqa \
         --mode train
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type frameqa \
         --mode test
 
-python3 preprocess/appearance_features.py \
+python preprocess/appearance_features.py \
         --gpu_id 2 \
         --dataset tgif-qa \
         --question_type frameqa \
         --feature_type pool5 \
         --num_frames 16
 
-python3 preprocess/motion_features.py \
+python preprocess/motion_features.py \
         --gpu_id 2 \
         --dataset tgif-qa \
         --question_type frameqa \
