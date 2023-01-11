05d4ed2a3442cb4b677679ea054fc93aa0b90ef5
sudoku <gzcsudo@gmail.com>
2023-01-11 18:49:58 +0800

    Update msvd.sh

--- pre_sh/msvd.sh
+++ pre_sh/msvd.sh
@@ -1,31 +1,31 @@
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msvd-qa \
         --question_type none \
         --mode total
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msvd-qa \
         --question_type none \
         --mode train
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msvd-qa \
         --question_type none \
         --mode val
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msvd-qa \
         --question_type none \
         --mode test
 
-python3 preprocess/appearance_features.py \
+python preprocess/appearance_features.py \
         --gpu_id 0 \
         --dataset msvd-qa \
         --question_type none \
         --feature_type pool5 \
         --num_frames 16
 
-python3 preprocess/motion_features.py \
+python preprocess/motion_features.py \
         --gpu_id 0 \
         --dataset msvd-qa \
         --question_type none \
