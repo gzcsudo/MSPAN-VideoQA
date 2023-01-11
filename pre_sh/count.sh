cbc55bb5b1c391a22704fca2e49435f4d6104349
sudoku <gzcsudo@gmail.com>
2023-01-11 18:48:50 +0800

    Update count.sh

--- pre_sh/count.sh
+++ pre_sh/count.sh
@@ -1,26 +1,26 @@
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type count \
         --mode total
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type count \
         --mode train
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type count \
         --mode test
 
-python3 preprocess/appearance_features.py \
+python preprocess/appearance_features.py \
         --gpu_id 1 \
         --dataset tgif-qa \
         --question_type count \
         --feature_type pool5 \
         --num_frames 16
 
-python3 preprocess/motion_features.py \
+python preprocess/motion_features.py \
         --gpu_id 1 \
         --dataset tgif-qa \
         --question_type count \
