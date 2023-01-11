eac4e5cdb2a383d8ea8f10b87d8ba15b8b31dbda
sudoku <gzcsudo@gmail.com>
2023-01-11 18:49:37 +0800

    Update msrvtt.sh

--- pre_sh/msrvtt.sh
+++ pre_sh/msrvtt.sh
@@ -1,31 +1,31 @@
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msrvtt-qa \
         --question_type none \
         --mode total
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msrvtt-qa \
         --question_type none \
         --mode train
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msrvtt-qa \
         --question_type none \
         --mode val
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset msrvtt-qa \
         --question_type none \
         --mode test
 
-python3 preprocess/appearance_features.py \
+python preprocess/appearance_features.py \
         --gpu_id 1 \
         --dataset msrvtt-qa \
         --question_type none \
         --feature_type pool5 \
         --num_frames 16
 
-python3 preprocess/motion_features.py \
+python preprocess/motion_features.py \
         --gpu_id 1 \
         --dataset msrvtt-qa \
         --question_type none \
