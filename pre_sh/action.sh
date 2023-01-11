6aa75fec926ca3caa9c7bd1dbdadfc5f576814cb
sudoku <gzcsudo@gmail.com>
2023-01-11 18:48:29 +0800

    Update action.sh

--- pre_sh/action.sh
+++ pre_sh/action.sh
@@ -1,26 +1,26 @@
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type action \
         --mode total
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type action \
         --mode train
 
-python3 preprocess/question_features.py \
+python preprocess/question_features.py \
         --dataset tgif-qa \
         --question_type action \
         --mode test
 
-python3 preprocess/appearance_features.py \
+python preprocess/appearance_features.py \
         --gpu_id 0 \
         --dataset tgif-qa \
         --question_type action \
         --feature_type pool5 \
         --num_frames 16
 
-python3 preprocess/motion_features.py \
+python preprocess/motion_features.py \
         --gpu_id 0 \
         --dataset tgif-qa \
         --question_type action \
